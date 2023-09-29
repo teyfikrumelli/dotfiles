d9590c6834b5ceceb700c3657994809a268695db
teyfikrumelli <teyfik.rumelli@gmail.com>
2023-09-29 23:28:13 +0200

    add keymaps

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -3,6 +3,13 @@ vim.g.mapleader = " "
 local keymap = vim.keymap
 
 -- general keymaps
+keymap.set("n", "<leader>y", "\"0y")
+keymap.set("v", "<leader>y", "\"0y")
+keymap.set("n", "<leader>p", "\"0p")
+keymap.set("v", "<leader>p", "\"0p")
+keymap.set("n", "<leader>d", "\"_d")
+keymap.set("v", "<leader>d", "\"_d")
+
 keymap.set("n", "<leader>nh", ":nohl<CR>")     -- clear search bar
 
 keymap.set("n", "<leader>sv", "<C-w>v")        -- split window vertically
@@ -21,11 +28,11 @@ keymap.set("n", "<leader>tp", ":tabp<CR>")     --  go to previous tab
 keymap.set("n", "<leader>e", ":Neotree toggle<CR>") -- toggle file explorer
 
 -- telescope
-keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
-keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>")                      -- find string in current working directory as you type
-keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>")                    -- find string under cursor in current working directory
-keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>")                        -- list open buffers in current neovim instance
-keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>")                      -- list available help tags
+keymap.set("n", "<leader>ff", "<cmd>Telescope find_files find_command=rg,--ignore,--hidden,--files<cr>") -- find files within current working directory, respects .gitignore
+keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>")                                            -- find string in current working directory as you type
+keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>")                                          -- find string under cursor in current working directory
+keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>")                                              -- list open buffers in current neovim instance
+keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>")                                            -- list available help tags
 
 -- neogit
 vim.keymap.set("n", "<leader>n", "<cmd>Neogit<cr>")
@@ -47,3 +54,6 @@ keymap.set("n", "<leader>z", "<cmd>ZenMode<cr>")
 
 -- undo tree
 keymap.set("n", "<leader>u", "<cmd>UndotreeToggle<cr>")
+
+-- golang
+keymap.set("n", "<leader>ee", "oif err != nil {<cr>}<esc>Oreturn err")
--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -1,1 +1,2 @@
 export FZF_DEFAULT_OPTS="--layout=reverse --inline-info"
+export FZF_DEFAULT_COMMAND="rg --hidden --ignore --files"
