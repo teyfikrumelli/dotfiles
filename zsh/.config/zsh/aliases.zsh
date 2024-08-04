6c1943209d892be1176453eed6bdd94b6ac33452
teyfikrumelli <teyfik.rumelli@gmail.com>
2024-08-04 22:44:45 +0200

    update zsh aliases

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -1,6 +1,6 @@
-alias ls="exa --icons"
-alias ll="exa -l -g --icons"
-alias llt="exa -1 --icons --tree"
+alias ls="eza --icons"
+alias ll="eza -l -g --icons"
+alias llt="eza -1 --icons --tree"
 
-alias cat="batcat"
+alias cat="bat"
 alias search="fzf --preview 'bat --color=always --style=numbers --line-range=:500 {}' | xargs nvim"
