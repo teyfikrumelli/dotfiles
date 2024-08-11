1eef054e2555c179638741a81ba11c7117850da2
teyfikrumelli <teyfik.rumelli@gmail.com>
2024-08-11 23:48:37 +0200

    fix cat alias

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -2,5 +2,5 @@ alias ls="eza --icons"
 alias ll="eza -l -g --icons"
 alias llt="eza -1 --icons --tree"
 
-alias cat="bat"
+alias cat="batcat"
 alias search="fzf --preview 'bat --color=always --style=numbers --line-range=:500 {}' | xargs nvim"
