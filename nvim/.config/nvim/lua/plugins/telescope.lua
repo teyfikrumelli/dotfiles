bc6ce6817a92bff0ab997328046dbab06b27a01d
teyfikrumelli <teyfik.rumelli@deliveryhero.com>
2025-03-20 16:45:32 +0100

    upgrades

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -13,6 +13,17 @@ telescope.setup({
 			},
 		},
 	},
+  pickers = {
+    find_files = {
+      hidden = true
+    },
+    grep_string = {
+      additional_args = {"--hidden","--no-ignore"}
+    },
+    live_grep = {
+      additional_args = {"--hidden","--no-ignore"}
+    },
+  },
 })
 
 telescope.load_extension("fzf")
